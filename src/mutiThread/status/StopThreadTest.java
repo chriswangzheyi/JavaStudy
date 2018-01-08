42f1a83c217c43f11b5c120dca263bc8dcf2ee06
ZheyiWang <123348687@qq.com>
2018-01-08 21:48:09 +0800

    java学习多线程
    java学习多线程

---
+++ src/mutiThread/status/StopThreadTest.java
@@ -0,0 +1,45 @@
+package mutiThread.status;
+
+public class StopThreadTest implements Runnable{
+	
+	private boolean stopFlag = true;
+	
+	public void run() {
+		int i=0;
+		while(stopFlag){
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			i++;
+			System.out.println(Thread.currentThread().getName()+"----runing i=" + i);
+		}
+	}
+	
+	public void stopThread(){
+		stopFlag = false;
+	}
+	
+	public static void main(String[] args) {
+		
+		int index =0;
+		StopThreadTest threadStop =  new StopThreadTest();
+	    new Thread(threadStop).start(); 		
+		while(true){
+			try {
+				Thread.sleep(20);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			index++;
+			System.out.println("*****************" + Thread.currentThread().getName()+"--------"+index);
+			if(index == 20){
+				threadStop.stopThread();				
+				break;
+			}			
+		}
+	}
+
+	
+}
